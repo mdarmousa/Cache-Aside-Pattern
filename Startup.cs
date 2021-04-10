494c83e2c24d2770951d9a85104edd8217931f81
Mohammad Darmousa <mdarmousa1@gmail.com>
2021-04-10 22:55:32 +0300

    Add Microsoft.Extensions.Caching.Memory

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace Cache_Aside_Pattern
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMemoryCache();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
